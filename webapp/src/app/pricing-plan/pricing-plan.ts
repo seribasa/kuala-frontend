e5b185ecaf1a56ae22b4849c48ae2924836a70a1
Ade Adi Wibowo <ade.adiw@gmail.com>
2025-08-25 22:44:24 +0200

    Code base for new Angular Kuala Frontend

---
+++ webapp/src/app/pricing-plan/pricing-plan.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-pricing-plan',
+  imports: [],
+  templateUrl: './pricing-plan.html',
+  styleUrl: './pricing-plan.css'
+})
+export class PricingPlan {
+
+  constructor(private router: Router) {
+  }
+
+  goToLogin(): void {
+  }
+
+}
